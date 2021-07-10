cebff37ee1f951f6a11b43cd3279fc609fa9866d
Alicia Moreno <lisireyes93@yahoo.com>
2021-07-10 14:38:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let friends = ["Luis", "Becca", "Jessica", "Jazmin"]
+
+let party = []
+
+function writeCards(people, goldfishBirthday) {
+    for (let f = 0; f < people.length; f++){
+        party.push(`Thank you, ${people[f]}, for the wonderful ${goldfishBirthday} gift!`)   
+    }
+    return party;
+}
+
+let hello= 10;
+
+function countDown(number){
+    while (number > -1) {
+        console.log(number--);
+        
+    }
+ 
+}
+
